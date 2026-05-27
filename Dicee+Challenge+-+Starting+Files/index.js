55a8a1e44c94df02010d436e701316b7c477bd9c
Harshitbyte1 <harshit.kandasamy@gmail.com>
2026-05-27 09:36:27 +0530

    bootstrap

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,26 @@
+// Player 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+// Change image for Player 1
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage1;
+
+document.querySelector(".img1").setAttribute("src", randomImageSource1);
+
+
+// Player 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// Change image for Player 2
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+document.querySelector(".img2").setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
